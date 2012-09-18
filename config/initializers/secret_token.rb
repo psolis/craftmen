4262ba872f5ae2d62cea849f7eddae08496ae8b8
psolis <josolis@pernix-solutions.com>
2012-09-18 11:38:04 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craftmen::Application.config.secret_token = '9bd2c05c914953cdd81cabf7a5ff17c03fdef2efcb2adac88822282d640bbbff08e3e814081213e0a316463d2e7418111303094d0c6587c2b7c05c9fa7664aeb'
